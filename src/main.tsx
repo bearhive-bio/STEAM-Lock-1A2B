fbe87f4d1f365c2d80e88e7fc93cb485476108ce
bearhive-bio <wolfskinwang@gmail.com>
2025-12-08 23:04:53 +0800

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx' // 這裡會引入您的遊戲主程式
+import './index.css' // 這裡會引入 Tailwind 的設定(如果有)
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
